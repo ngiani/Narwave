3669f780e3ec86f666bcb5933dd368b14bb354ba
Federico Aliprandi <fede.alip@gmail.com>
2017-01-21 10:33:57 +0100

    Imported waves scripts

---
+++ Assets/Project/Scripts/Sinus.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sinus : MonoBehaviour {
+
+	public int numPoints;
+	[Range(-10f,10f)]
+	public float height;
+	public float startPosY;
+
+	LineRenderer line;
+
+	// Use this for initialization
+	void Start () {
+		line = GetComponent<LineRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		line.numPositions = numPoints;
+		float distance = 2*Mathf.PI/numPoints;
+		for (int i = 0; i < line.numPositions; i++)
+		{
+			line.SetPosition(i, new Vector3(i*distance, height*Mathf.Sin(i*distance) + startPosY, 0f));
+		}
+	}
+}
---
+++ Assets/Project/Scripts/SumWave.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumWave : MonoBehaviour {
+
+	public Sinus linePlayer1, linePlayer2;
+
+	private LineRenderer line;
+	private int numPoints;
+	private float startPosY;
+
+	// Use this for initialization
+	void Start () {
+		line = GetComponent<LineRenderer>();
+		numPoints = linePlayer1.numPoints;
+		line.numPositions = numPoints;
+		linePlayer2.numPoints = numPoints;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float distance = 2*Mathf.PI/numPoints;
+		float sum;
+		startPosY = (linePlayer1.startPosY + linePlayer2.startPosY)/2;
+
+		for (int i = 0; i < line.numPositions; i++)
+		{
+			sum = linePlayer1.GetComponent<LineRenderer>().GetPosition(i).y + linePlayer2.GetComponent<LineRenderer>().GetPosition(i).y  - (linePlayer1.startPosY + linePlayer2.startPosY); 
+
+			Debug.Log(linePlayer1.GetComponent<LineRenderer>().GetPosition(i).y + " + " + linePlayer1.GetComponent<LineRenderer>().GetPosition(i).y + " - " + (linePlayer1.startPosY + linePlayer2.startPosY) + " = " + sum);
+
+			line.SetPosition(i, new Vector3(i*distance, sum + startPosY, 0f));
+		}
+	}
+}
