1e0b6f6a131f8acdc757e6b22836148a50360931
Federico Aliprandi <fede.alip@gmail.com>
2017-01-21 12:27:57 +0100

    LaserWave

---
+++ Assets/Project/Scripts/LaserWave.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LaserWave : MonoBehaviour
+{
+    /// <summary>
+    /// The number of complete texture offsets done in 1 second.
+    /// </summary>
+    public float moveFrequency = 10f;
+
+    private float currentTextureOffset = 0f;
+
+    private LineRenderer lineRenderer;
+    private Material material;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        material = lineRenderer.material;
+    }
+
+    private void Update()
+    {
+        currentTextureOffset = (currentTextureOffset - moveFrequency * Time.deltaTime) % 1f;
+        material.mainTextureOffset = new Vector2(currentTextureOffset, 0f);
+    }
+}
