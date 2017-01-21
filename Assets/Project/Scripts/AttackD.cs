543ad1851ad26021232f8eac26e6e773353c30c1
pc-federico\Federico <f.distefano87@hotmail.it>
2017-01-22 00:46:42 +0100

    fatta LA MANTA!

---
+++ Assets/Project/Scripts/AttackD.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackD : MonoBehaviour
+{
+    public Monster monsterBulletD;
+    private float timerStart;
+    public float timerFire;
+    public float timerStopFire;
+    private float timerStopFireInt;
+
+    void Start()
+    {
+        timerStart = Time.time;
+        timerStopFireInt = Time.time + timerStopFire;
+    }
+
+    void Update()
+    {
+        //SPARA
+        if (Time.time >= timerStart + timerFire && Time.time <= timerStopFireInt)
+        {
+            timerStart = Time.time;
+            monsterBulletD.Spawn(transform.position + new Vector3(0.0f, -1.0f, 0f));
+        }
+    }
+}
---
+++ Assets/Project/Scripts/MovementBulletD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBulletD : MonoBehaviour
+{
+    public float speed;
+    //Vector3 pos;
+    Vector3 translation;
+
+    void OnEnable()
+    {
+        translation = new Vector3(0f, 0f, 0f);
+    }
+
+    void Update()
+    {
+        //MOVIMENTO
+        Vector2 translation = Vector2.down;
+        gameObject.transform.Translate(translation * speed * Time.deltaTime);
+    }
+}
