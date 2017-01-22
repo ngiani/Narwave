8c751286e1486b169d1bc7061e8c1f12e4c1445f
pc-federico\Federico <f.distefano87@hotmail.it>
2017-01-22 02:25:22 +0100

    level manager fix in progress

--- Assets/Project/Scripts/LevelManager.cs
+++ Assets/Project/Scripts/LevelManager.cs
@@ -15,6 +15,10 @@ public class LevelManager : MonoBehaviour
     private Vector3 spawnPoint2;
     private Vector3 realSpawnPoint;
 
+    private int wave;
+    private float timerStartWave;
+    private bool fineGame;
+
     //DEBUG
     //bool bbb = true;
     //FINE DEBUG
@@ -25,36 +29,33 @@ public class LevelManager : MonoBehaviour
         Camera camera = Camera.main;
         spawnPoint1 = camera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
         spawnPoint2 = camera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f));
+
+        timerStartWave = Time.time;
+        wave = 1;
+        fineGame = false;
     }
 	
 	void Update ()
     {
+        while(!fineGame)
+        {
+            if (wave == 1)
+            {
+                if (Time.time >= timerStart + timerSpawn)
+                {
+                    float randomY = Random.Range(spawnPoint2.y, spawnPoint1.y);
+                    realSpawnPoint = new Vector3(spawnPoint1.x, randomY, 0f);
+                    monsterD.Spawn(realSpawnPoint);
+
+                    timerStart = Time.time;
+                }
+            }
+
+        }
+
 
         
-
-
-        //TEST SPAWN MOSTRI
-        if (Time.time >= timerStart + timerSpawn)
-        {
-            float randomY = Random.Range(spawnPoint2.y, spawnPoint1.y);
-            realSpawnPoint = new Vector3(spawnPoint1.x, randomY, 0f);
-            monsterD.Spawn(realSpawnPoint);
-
-            timerStart = Time.time;
-        }
-
-        /*
-        //DEBUG
-        if (bbb)
-        {
-            bbb = false;
-            float randomYY = Random.Range(spawnPoint2.y, spawnPoint1.y);
-            realSpawnPoint = new Vector3(spawnPoint1.x, randomYY, 0f);
-            timerStart = Time.time;
-            monsterA.Spawn(realSpawnPoint); 
-        }
-        // FINE DEBUG
-        */
+        
 
     }
 
